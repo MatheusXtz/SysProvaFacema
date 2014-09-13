7b14f4f377e8450754d096c719841d5abc7588b9
Clesyo Silva <Clesyo Silva@ClesyoMoreira>
2014-09-13 06:42:22 -0300

    add comentario na class application

--- SysProva/app/controllers/Application.java
+++ SysProva/app/controllers/Application.java
@@ -6,7 +6,7 @@ import play.mvc.*;
 import views.html.*;
 
 public class Application extends Controller {
-
+//	Metodo da pagina principal
     public static Result index() {
         return ok(index.render("Your new application is ready."));
     }
