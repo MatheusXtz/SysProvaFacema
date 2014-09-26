b197c0a3242fd65091d46850ee6227a79e5b5c84
Clesyo Moreira da SIlva <clesyo.silva@gmail.com>
2014-09-26 10:32:04 -0300

    implementação da visão de login

--- SysProva/app/controllers/Application.java
+++ SysProva/app/controllers/Application.java
@@ -1,18 +1,24 @@
 package controllers;
 
+import models.Usuario;
 import play.*;
+import play.data.Form;
 import play.mvc.*;
-
 import views.html.*;
 
 public class Application extends Controller {
 	// Metodo da pagina principal
+	 private static final Form<Usuario> formUser = Form.form(Usuario.class);
 	public static Result index() {
 		return TODO;
 	}
 
 	public static Result inicio() {
 		return ok(index.render());
+	}
+	
+	public static Result login(){
+		return ok(login.render(formUser));
 	}
 
 }
